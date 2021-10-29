ed59ff393c653db43fea5366db8d93e7148fe427
isaiah wright <s10780369@students.duvalschools.org>
2021-10-29 14:43:00 -0400

    Update variabler_practice.py

--- variabler_practice.py
+++ variabler_practice.py
@@ -1,1 +1,11 @@
 # Variable Practice, Isaiah Wright, 10/29/21 2:22pm, 0.0
+
+# Three common Types of Data
+
+lives_remaining = 9000 #INTERGERS, any +/- WHOLE number and 0 
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal
+death_cry = "I'm The Strongest In The Universe!"
+# Line 7 is a STRING ~ ~ collection of letters/numbers/other chars.
+
+print(lives_remaining)
+
